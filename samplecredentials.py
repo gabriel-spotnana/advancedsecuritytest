f441069ca47d7988b4ce6e9b3f5d11891d369310
gabriel-spotnana <111281246+gabriel-spotnana@users.noreply.github.com>
2023-07-11 15:15:39 +0300

    Update samplecredentials.py

--- samplecredentials.py
+++ samplecredentials.py
@@ -1,11 +1,11 @@
-aws_access_key_id AKIAZKF5IKZG8IIFABC  
-aws_secret_access_key 7ydunEQ2HLI2dZSAjPORCbU3z1KYF3Sicdtu/9Zi
+aws_access_key_id="AKIAZKF5IKZG8IIFABC"  
+aws_secret_access_key= "7ydunEQ2HLI2dZSAjPORCbU3z1KYF3Sicdtu/9Zi/"
 
-DB_USERNAME=admin
-DB_PASSWORD=SuperSecretPassword123
-DB_HOST=localhost
-DB_PORT=5432
-DB_NAME=mydatabase
+DB_USERNAME="admin"
+DB_PASSWORD="SuperSecretPassword123"
+DB_HOST="localhost"
+DB_PORT="5432"
+DB_NAME="mydatabase"
 
 
-ACCESS_TOKEN=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c
+ACCESS_TOKEN="eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c"

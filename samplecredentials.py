fec400a09c62d7871d3f482de8e6a30b7d86492a
gabriel-spotnana <gabriel@spotnana.com>
2023-07-18 17:39:46 +0300

    Update samplecredentials.py

--- samplecredentials.py
+++ samplecredentials.py
@@ -9,3 +9,16 @@ DB_NAME="mydatabase"
 
 
 ACCESS_TOKEN="eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c"
+
+
+aws_access_key_id="AKIAZKF3IKZG8IIFABC"  
+aws_secret_access_key= "7ysunEQ2HLI2dZSAjPORCbU3z1KYF3Sicdtu/9Zi/"
+
+DB_USERNAME="admi1n"
+DB_PASSWORD="SuperSecretPassword1123"
+DB_HOST="localhost"
+DB_PORT="5432"
+DB_NAME="mydatabase"
+
+
+ACCESS_TOKEN="eyJhbGcsOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c"
